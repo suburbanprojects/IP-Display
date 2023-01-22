3ee3d91c792d3452c92079625f57dd0452fae7be
suburbanprojects <96915573+suburbanprojects@users.noreply.github.com>
2023-01-22 23:09:12 +0800

    Update IPDisplayGUI.py

--- IPDisplayGUI.py
+++ IPDisplayGUI.py
@@ -1,22 +1,29 @@
 import socket
 import urllib.request
 import tkinter as tk
+import pyperclip
 
 app =tk.Tk()
 app.title("IP Display")
+app.geometry("350x150")
 
 def showIP():
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     localIP = s.getsockname()[0]
-    
+    EntryHomeIP.delete(0, "end")
     EntryHomeIP.insert(tk.INSERT, localIP)
-    EntryHomeIP.config(state='disabled')
 
-    publicip= urllib.request.urlopen('http://myip.dnsomatic.com').read().decode('utf8')
+    publicip= urllib.request.urlopen('https://v4.ident.me/').read().decode('utf8')
+    EntryExtIP.delete(0, "end")
     EntryExtIP.insert(tk.INSERT, publicip)
-    EntryExtIP.config(state='disabled')
+
+def CopyPass1():
+    pyperclip.copy(EntryHomeIP.get())
+
+def CopyPass2():
+    pyperclip.copy(EntryExtIP.get())
 
 HomeIP = tk.Label(app, text="Local IP")
 HomeIP.grid(column = 0, row=0, ipadx=5, pady=5, sticky=tk.W+tk.N)
@@ -30,7 +37,11 @@ EntryExtIP.grid(column=1,row=1,padx=10,pady=5, sticky=tk.S)
 
 resultButton = tk.Button(app, text='Show IP', command=showIP)
 resultButton.grid(column=0, row=4, pady=10, sticky=tk.W)
+resultCopy1 = tk.Button(app, text='Copy', command=CopyPass1)
+resultCopy1.grid(column=2, row=0, pady=10, sticky=tk.W)
+resultCopy2= tk.Button(app, text='Copy', command=CopyPass2)
+resultCopy2.grid(column=2, row=1, pady=10, sticky=tk.W)
 resultQuit = tk.Button(app, text='Quit', command=app.quit)
-resultQuit.grid(column=1, row=4, pady=10, sticky=tk.W)
+resultQuit.grid(column=2, row=4, pady=10, sticky=tk.W)
 
 app.mainloop()
